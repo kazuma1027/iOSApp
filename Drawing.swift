8bb15db1564319412227c8b37ba30ad0d425ef34
Mazuka <cgub1063@mail4.doshisha.ac.jp>
2020-12-01 20:35:35 +0900

    Test

---
+++ Drawing.swift
@@ -0,0 +1,14 @@
+//
+//  Drawing.swift
+//  FoodTracker_2
+//
+//  Created by 竹内一真 on 2020/12/01.
+//
+
+import CoreGraphics
+import UIKit
+
+struct Drawing {
+    var color = UIColor.black
+    var points = [CGPoint]()
+}
